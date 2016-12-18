8423929722630274a055e65076c6f0fef1e4a87e
Cowerling <Cowerling@gmail.com>
2016-12-18 23:17:38 +0800

    Aspect-oriented Spring at home

---
+++ src/com/cowerling/concert/Audience.java
@@ -0,0 +1,32 @@
+package com.cowerling.concert;
+
+import org.aspectj.lang.annotation.*;
+
+/**
+ * Created by cowerling on 16-12-18.
+ */
+@Aspect
+public class Audience {
+    @Pointcut("execution(**com.cowerling.concert.Performance.perform(..))")
+    public void performance() {}
+
+    @Before("performance()")
+    public void silenceCellPhones() {
+        System.out.println("Silencing cell phones");
+    }
+
+    @Before("performance()")
+    public void takeSeats() {
+        System.out.println("Taking seats");
+    }
+
+    @AfterReturning("performance()")
+    public void applause() {
+        System.out.println("CLAP CLAP CLAP!!!");
+    }
+
+    @AfterThrowing("performance()")
+    public void demandRefund() {
+        System.out.println("Demanding a refund");
+    }
+}
---
+++ src/com/cowerling/concert/Performance.java
@@ -0,0 +1,8 @@
+package com.cowerling.concert;
+
+/**
+ * Created by cowerling on 16-12-18.
+ */
+public interface Performance {
+    public void perform();
+}
