0174268294dced21d3484c7a4d0101f2c37699a9
Cowerling <Cowerling@gmail.com>
2016-12-26 22:23:01 +0800

    Aspect-oriented Spring at home

--- src/com/cowerling/soundsystem/BlankDisc.java
+++ src/com/cowerling/soundsystem/BlankDisc.java
@@ -9,6 +9,8 @@ public class BlankDisc implements CompactDisc {
     private String artist;
     private List<String> tracks;
 
+    public BlankDisc() {}
+
     public BlankDisc(String title, String artist) {
         this.title = title;
         this.artist = artist;
@@ -24,6 +26,10 @@ public class BlankDisc implements CompactDisc {
 
     public void setTracks(List<String> tracks) {
         this.tracks = tracks;
+    }
+
+    public void playTrack(int trackNumber) {
+        System.out.println("-Track: " + tracks.get(trackNumber - 1));
     }
 
     public void play() {
--- src/com/cowerling/soundsystem/CompactDisc.java
+++ src/com/cowerling/soundsystem/CompactDisc.java
@@ -4,5 +4,6 @@ package com.cowerling.soundsystem;
  * Created by cowerling on 16-11-20.
  */
 public interface CompactDisc {
+    void playTrack(int trackNumber);
     void play();
 }
